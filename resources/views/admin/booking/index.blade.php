6508ab99828a1efe8a352ef68cd449db05e2a820
mochammadazizromdhoni <mochammadazizr@gmail.com>
2026-02-11 10:21:06 +0700

    update index bagian admin

--- resources/views/admin/booking/index.blade.php
+++ resources/views/admin/booking/index.blade.php
@@ -11,84 +11,72 @@
 
 <!-- TABEL BOOKING -->
 <div class="bg-white rounded shadow overflow-x-auto">
-    <table class="w-full text-sm">
-        <thead class="bg-red-700 text-white">
-            <tr>
-                <th class="p-3">No</th>
-                <th class="p-3">Nama Pemesan</th>
-                <th class="p-3">Lapangan</th>
-                <th class="p-3">Tanggal</th>
-                <th class="p-3">Jam</th>
-                <th class="p-3">Durasi</th>
-                <th class="p-3">Pembayaran</th>
-                <th class="p-3">Status</th>
-                <th class="p-3">Aksi</th>
-            </tr>
-        </thead>
+    <table class="w-full text-sm border-collapse">
+    <thead class="bg-red-700 text-white">
+        <tr>
+            <th class="px-4 py-3 text-center">No</th>
+            <th class="px-4 py-3 text-left">Nama Pemesan</th>
+            <th class="px-4 py-3 text-left">Lapangan</th>
+            <th class="px-4 py-3 text-center">Tanggal</th>
+            <th class="px-4 py-3 text-center">Jam</th>
+            <th class="px-4 py-3 text-center">Durasi</th>
+            <th class="px-4 py-3 text-center">Pembayaran</th>
+            <th class="px-4 py-3 text-center">Status</th>
+            <th class="px-4 py-3 text-center">Aksi</th>
+        </tr>
+    </thead>
 
-        <tbody class="text-gray-700">
+    <tbody class="text-gray-700">
+        <tr class="border-b hover:bg-gray-100">
+            <td class="px-4 py-3 text-center">1</td>
+            <td class="px-4 py-3">Fauzi</td>
+            <td class="px-4 py-3">Lapangan A</td>
+            <td class="px-4 py-3 text-center">2026-02-12</td>
+            <td class="px-4 py-3 text-center">19.00</td>
+            <td class="px-4 py-3 text-center">2 Jam</td>
+            <td class="px-4 py-3 text-center">QRIS</td>
+            <td class="px-4 py-3 text-center">
+                <span class="inline-block px-3 py-1 bg-yellow-500 text-white rounded-full text-xs">
+                    Pending
+                </span>
+            </td>
+            <td class="px-4 py-3 text-center space-x-1">
+                <button class="bg-green-600 hover:bg-green-700 text-white px-3 py-1 rounded text-xs">
+                    Konfirmasi
+                </button>
+                <button class="bg-red-600 hover:bg-red-700 text-white px-3 py-1 rounded text-xs">
+                    Batal
+                </button>
+            </td>
+        </tr>
+    </tbody>
 
-            <tr class="border-b hover:bg-gray-100">
-                <td class="p-3 text-center">1</td>
-                <td class="p-3">Fauzi</td>
-                <td class="p-3">Lapangan A</td>
-                <td class="p-3">2026-02-12</td>
-                <td class="p-3">19.00</td>
-                <td class="p-3">2 Jam</td>
-                <td class="p-3">QRIS</td>
-                <td class="p-3">
-                    <span class="px-2 py-1 bg-yellow-500 text-white rounded text-xs">
-                        Pending
-                    </span>
-                </td>
-                <td class="p-3 space-x-1">
-                    <button class="bg-green-600 text-white px-2 py-1 rounded text-xs">
-                        Konfirmasi
-                    </button>
-                    <button class="bg-red-600 text-white px-2 py-1 rounded text-xs">
-                        Batal
-                    </button>
-                </td>
-            </tr>
+    <tbody class="text-gray-700">
+        <tr class="border-b hover:bg-gray-100">
+            <td class="px-4 py-3 text-center">1</td>
+            <td class="px-4 py-3">Bayu</td>
+            <td class="px-4 py-3">Lapangan A</td>
+            <td class="px-4 py-3 text-center">2026-02-12</td>
+            <td class="px-4 py-3 text-center">21.00</td>
+            <td class="px-4 py-3 text-center">1 Jam</td>
+            <td class="px-4 py-3 text-center">QRIS</td>
+            <td class="px-4 py-3 text-center">
+                <span class="inline-block px-3 py-1 bg-green-500 text-white rounded-full text-xs">
+                    Selesai
+                </span>
+            </td>
+            <td class="px-4 py-3 text-center space-x-1">
+                <button class="bg-green-600 hover:bg-green-700 text-white px-3 py-1 rounded text-xs">
+                    Konfirmasi
+                </button>
+                <button class="bg-red-600 hover:bg-red-700 text-white px-3 py-1 rounded text-xs">
+                    Batal
+                </button>
+            </td>
+        </tr>
+    </tbody>
+</table>
 
-            <tr class="border-b hover:bg-gray-100">
-                <td class="p-3 text-center">2</td>
-                <td class="p-3">Bayu</td>
-                <td class="p-3">Lapangan B</td>
-                <td class="p-3">2026-02-12</td>
-                <td class="p-3">20.00</td>
-                <td class="p-3">1 Jam</td>
-                <td class="p-3">Cash</td>
-                <td class="p-3">
-                    <span class="px-2 py-1 bg-green-600 text-white rounded text-xs">
-                        Selesai
-                    </span>
-                </td>
-                <td class="p-3">
-                    <span class="text-gray-400 text-xs">-</span>
-                </td>
-            </tr>
-
-            <tr class="hover:bg-gray-100">
-                <td class="p-3 text-center">3</td>
-                <td class="p-3">Dimas</td>
-                <td class="p-3">Lapangan A</td>
-                <td class="p-3">2026-02-13</td>
-                <td class="p-3">18.00</td>
-                <td class="p-3">2 Jam</td>
-                <td class="p-3">Transfer</td>
-                <td class="p-3">
-                    <span class="px-2 py-1 bg-red-600 text-white rounded text-xs">
-                        Dibatalkan
-                    </span>
-                </td>
-                <td class="p-3">
-                    <span class="text-gray-400 text-xs">-</span>
-                </td>
-            </tr>
-
-        </tbody>
-    </table>
 </div>
 
 @endsection
