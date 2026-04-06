022fb485a708e3f6b9b6f12f8aff783264a7a21e
JuraganEmpang <ferdinandardan@gmail.com>
2026-04-06 08:08:24 +0700

    messege baru

--- app/Http/Controllers/LapanganController.php
+++ app/Http/Controllers/LapanganController.php
@@ -3,67 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-<<<<<<< HEAD
 
-class LapanganController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
-}
-=======
 use App\Models\Lapangan;
 
 class LapanganController extends Controller
@@ -76,4 +16,3 @@ class LapanganController extends Controller
         return view('lapangan', compact('data_lapangan'));
     }
 }
->>>>>>> ff7f9356a078661112c9e5b3792b192e17c5b0e6
